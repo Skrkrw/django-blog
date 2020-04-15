c56d8147625179a171ec4535d03eb6d62445bda6
Jose Enciso <josejefloen@gmail.com>
2020-04-15 23:31:24 +0000

    Adding to heroku for deployment

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
+# House Keeping
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +25,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i0p02pns+gqjj#78-_$=d4_y8uejl@az!o=d7b#6)x4^!(-e$n'
+# SECRET_KEY in env
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'1dc8662a2c994be18a356a491b767a90.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = [u'1dc8662a2c994be18a356a491b767a90.vfs.cloud9.us-east-1.amazonaws.com', 'jose-blogs.herokuapp.com/']
 
 
 # Application definition - Serving Out the Templates
@@ -50,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNOiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_blog.urls'
@@ -77,13 +84,27 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
+# This is just to continue the integration testing with Travis working
+# Travis will not knoiw where the DATABES URL is
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) # Remeber to set the databes URL
+    }
+else:
+    print("Postgres URL not found. Using SQLit3 instead.")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -123,6 +144,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/' # Reference URL or label
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) # Actual/right directory
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/' # This is how Django refers to a URL and not to an specific directory
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Refering to the name of directory
